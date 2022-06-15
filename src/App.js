883a0ae520d3ab4a455b66db35da6940972f94a5
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-06-15 23:59:32 +0530

    added counter app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import React, { useState } from 'react';
 
 function App() {
+
+  const [value, setValue] = useState(0)
+  
+  const handleDec = ()=> {
+    if(value >0) setValue(value - 1)
+  }
+  const handleIncr = ()=> {
+    setValue(value + 1)
+  }
+
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{value}</h2>
+      <button data-testid="counter-decrement-button" onClick={()=> handleDec()}>Decrement</button>
+      <button data-testid="counter-increment-button" onClick={()=> handleIncr()}>Increment</button>
     </div>
   );
 }
